b72a25ed001d8b8808f56a4b61f6e2c6294ec7a4
ShardulVanage <vsharadul@gmail.com>
2024-12-28 11:47:21 +0530

    align top

--- src/app/layout.js
+++ src/app/layout.js
@@ -28,7 +28,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
 
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased mt-12`}
       >
         <Navbar/>
         {children}
